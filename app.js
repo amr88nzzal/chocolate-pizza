fe78cb74da2a8bbc7bce60b9f576df8f82a7634a
amr88nzzal <amr.nzzal@gmail.com>
2021-03-25 22:45:55 +0200

    final tutch

--- app.js
+++ app.js
@@ -2,13 +2,14 @@
 let form =document.getElementById('recipeForm');
 form.addEventListener('change',strictLine);
 function strictLine(event){
-  let checkBox1 = document.getElementById('checkbox1');
-  let lbl1 = document.getElementById('lbl1');
-  if(checkBox1.checked){
-    lbl1.style.textDecoration='line-through';
-    console.log(lbl1);
-  }
-  else{lbl1.style.textDecoration='unset';
-  }
+  for (let i=1 ;i<=12;i++){
+    let checkBox1 = document.getElementById('checkbox'+i);
+    let lbl1 = document.getElementById('lbl'+i);
+    if(checkBox1.checked){
+      lbl1.style.textDecoration='line-through';
+      console.log(lbl1);
+    }
+    else{lbl1.style.textDecoration='unset';
+    }}
 
 }
